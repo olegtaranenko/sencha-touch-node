d2c91e9ead9bee4fdfef541c810c6a9fac34fe05
Oleg Taranenko <olegtaranenko@gmail.com>
2016-02-27 19:15:02 +0100

    inial clone from node-extjs

---
+++ README.md
@@ -0,0 +1,48 @@
+# node-extjs
+
+Marriage Sencha Touch and Node.js 
+
+## Installation
+
+I cannot redistribute ExtJS with this module, so you will not be able to install it from npm repository directly 
+
+However, installation via npm is easily done manually: 
+
+1. Download Sencha Touch from https://www.sencha.com/products/touch
+2. Extract it
+3. Copy contents of `src/` into `lib/Ext/` in here
+4. `npm install .`
+
+
+## Usage
+
+Basically, all it takes is `require('extjs')`. <b>Warning:</b> a common mistake is to do `Ext=require('extjs')`. Don't. 
+
+See also `test/` folder for usage examples (you'll need nodeunit to run them).
+
+
+## Acknowledgments
+
+* Sencha Touch is (c) 2016 [Sencha](http://sencha.com/)
+
+
+## Thanks
+
+There several implementations of this idea, loading core Ext library inside NodeJs environment.
+
+  [n-ext](https://github.com/xcambar/n-ext) 
+  [touch-node](https://github.com/ModusCreateOrg/touch-node)
+  [node-extjs](git@github.com:egorFiNE/node-extjs)
+
+Thanks all for the idea and implementation.
+
+
+## License
+
+Do whatever you want with software as long as you are in compliance with [Sencha Touch license](https://www.sencha.com/legal/#Sencha_Touch). 
+
+
+## Author
+
+Oleg Taranenko <olegtaranenko@gmail.com>
+
---
+++ index.js
@@ -0,0 +1,58 @@
+Ext = {};
+
+global.__defineGetter__('document', function() {
+	return {
+		getElementsByTagName: function () {
+			return [{
+				src: '/'
+			}];
+		}
+	};
+});
+
+global.__defineGetter__('location', function() {
+	return '';
+});
+
+
+var ExtLoader = function() {
+	var extPath = __dirname + '/lib/Ext/';
+	var extCorePath = extPath + '/core/';
+
+	this.load = function () {
+		require(extCorePath+'Ext.js');
+		require(extCorePath+'version/Version.js');
+		require(extCorePath+'lang/Object.js');
+		require(extCorePath+'lang/Array.js');
+		require(extCorePath+'lang/Date.js');
+		require(extCorePath+'lang/Function.js');
+		require(extCorePath+'lang/Number.js');
+		require(extCorePath+'lang/String.js');
+		require(extCorePath+'class/Base.js');
+		require(extCorePath+'class/Class.js');
+		require(extCorePath+'class/ClassManager.js');
+		require(extCorePath+'lang/Error.js');
+		require(extCorePath+'class/Loader.js');
+
+		require(extPath+'DateExtras.js');
+		require(extPath+'util/Format.js');
+		require(extPath+'util/DelayedTask.js');
+		require(extPath+'event/Event.js');
+
+		require(extCorePath+'Ext-more.js');
+
+		Ext.Loader.config.enabled = true;
+		Ext.Loader.setPath('Ext', __dirname+'/lib/Ext');
+
+		global.__defineGetter__('window', function() {
+			return {};
+		});
+
+		Ext.require('Ext.data.ModelManager');
+		Ext.require('Ext.data.Store');
+
+	}
+};
+
+new ExtLoader().load();
+
